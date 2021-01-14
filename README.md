9581e86fb741a6a12fcc6d5f1e07456e61efae8c
Tsuruno <bluehearts@outlook.com>
2021-01-14 13:48:38 +0900

    complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # analogclock
+* * *
 ✨🕘✨
+* * *
+[see here analogclock](https://tsuruno.github.io/analogclock/analogclock.html)
+
+![Screenshot](screenshot.png)
--- clocksystem.js
+++ clocksystem.js
@@ -20,13 +20,24 @@ setInterval(() => {
   const elementS = document.querySelector(".c-clock__sec");
 
   var elementBody = document.getElementById("body");
+  var elementFrame = document.getElementById("frame");
+  var elementClock = document.getElementById("clock");
+
+  var elementBodycolor = document.getElementById("bodycolor");
+  var elementFramecolor = document.getElementById("framecolor");
+  var elementClockcolor = document.getElementById("clockcolor");
 
   // styleを追加
   elementH.style.transform = `rotate(${degH}deg)`;
   elementM.style.transform = `rotate(${degM}deg)`;
   elementS.style.transform = `rotate(${degS}deg)`;
 
-  // elementBody.style.backgroundColor = 'rgb(' +color +',' +color +',' +color +')';
   elementBody.style.backgroundColor = `rgb(${colorH},${colorM},${colorS})`;
+  elementFrame.style.backgroundColor = `rgb(${colorM},${colorS},${colorH})`;
+  elementClock.style.backgroundColor = `rgb(${colorS},${colorH},${colorM})`;
+
+  elementBodycolor.innerHTML = `Body:rgb(${colorH},${colorM},${colorS})`;
+  elementFramecolor.innerHTML = `Frame:rgb(${colorM},${colorS},${colorH})`;
+  elementClockcolor.innerHTML = `Clock:rgb(${colorS},${colorH},${colorM})`;
 
 }, 10);
